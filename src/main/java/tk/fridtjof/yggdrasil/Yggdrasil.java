46a7eff588c2db6b6e3ec6cd792857e72a1d18b0
Fridtjof-DE <fridtjof.goes@gmx.de>
2020-06-25 11:55:59 +0200

    Initial commit

---
+++ src/main/java/tk/fridtjof/yggdrasil/Yggdrasil.java
@@ -0,0 +1,17 @@
+package tk.fridtjof.yggdrasil;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Yggdrasil extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
